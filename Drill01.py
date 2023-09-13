3c8058dfa1d4203f6bf67a39feb2dddac5867bbf
2019182029_이원준 <S159753S@tukorea.ac.kr>
2023-09-13 21:57:04 +0900

    Drill01 제출 (2019182029 이원준)
    restart 함수 추가

--- Drill01.py
+++ Drill01.py
@@ -16,11 +16,15 @@ def turtle_right() :
     turtle.setheading(0)
     turtle.forward(50)
 
+def turtle_restart() :
+    turtle.reset()
+
 turtle.shape('turtle')
 
 turtle.onkey(turtle_up, 'w')
 turtle.onkey(turtle_left, 'a')
 turtle.onkey(turtle_down, 's')
 turtle.onkey(turtle_right, 'd')
+turtle.onkey(turtle_restart, 'Escape')
 
 turtle.listen()
