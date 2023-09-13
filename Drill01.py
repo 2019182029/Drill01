2429e9276b183296420c4ff39c1fd1b02988970b
2019182029_이원준 <S159753S@tukorea.ac.kr>
2023-09-13 21:48:03 +0900

    상하좌우 함수
    거북이의 상하좌우 움직임을 수행하는 함수

--- Drill01.py
+++ Drill01.py
@@ -1,2 +1,18 @@
 import turtle
 
+def turtle_up() :
+    turtle.setheading(90)
+    turtle.forward(50)
+    
+def turtle_left() :
+    turtle.setheading(1800)
+    turtle.forward(50)
+    
+def turtle_down() :
+    turtle.setheading(-90)
+    turtle.forward(50)
+    
+def turtle_right() :
+    turtle.setheading(0)
+    turtle.forward(50)
+
